0fc7e42cb2894c3676be29bf77b54bdaf45eb36b
PAWAN KUMAR SHUKLA <pawansam13@gmail.com>
2016-04-21 17:33:20 +0530

    small changes

--- index.php
+++ index.php
@@ -36,16 +36,10 @@ if(isset($_GET['logout'])){
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Chat - Customer Module</title>
-<link type="text/css" rel="stylesheet" href="style.css" />
-</head>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Chat - Customer Module</title>
+<title>Chat </title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 <?php
